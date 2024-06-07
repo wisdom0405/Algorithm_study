9243034c0805bbf6db367121e25d8a74fdaa08de
jeong seul ki <135789383+wisdom0405@users.noreply.github.com>
2024-06-07 12:16:31 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.09 ms, Memory: 69.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -0,0 +1,12 @@
+import java.util.regex.Pattern;
+class Solution {
+    public boolean solution(String s) {
+        
+        boolean answer = false;
+        if((s.length() == 4 || s.length() == 6) && s.matches("[0-9]+")){
+            answer = true;
+        }
+        
+        return answer;
+    }
+}
