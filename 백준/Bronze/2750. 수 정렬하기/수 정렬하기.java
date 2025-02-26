887c6cf885b1b23a2a5b286c95c351c6737038f1
jeong seul ki <135789383+wisdom0405@users.noreply.github.com>
2025-02-26 17:32:09 +0900

    [Bronze II] Title: 수 정렬하기, Time: 128 ms, Memory: 14544 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -1,3 +1,4 @@
+
 import java.io.*;
 import java.util.StringTokenizer;
 
@@ -6,31 +7,40 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
-        int N = Integer.parseInt(br.readLine()); // N : 숫자의 개수
+        StringTokenizer st = new StringTokenizer(br.readLine()); // 1번째 줄 읽음
+
+        int N = Integer.parseInt(st.nextToken()); // N : 숫자의 개수
         int[] A = new int[N]; // 크기 N의 배열 선언
+        int swapCount = 0; // swap count
 
         // 원본배열에 값 넣기
-        for (int i = 0; i < N; i++) {
+        for(int i=0; i<N; i++){
             A[i] = Integer.parseInt(br.readLine());
         }
 
-        // 버블 정렬 구현
-        for (int i = 0; i < N - 1; i++) {
-            for (int j = 0; j < N - 1 - i; j++) {
-                if (A[j] > A[j + 1]) {
-                    // 인접한 원소 교환
-                    int temp = A[j];
-                    A[j] = A[j + 1];
-                    A[j + 1] = temp;
+        // 버블정렬을 구현하는 영역
+        for(int i=0; i<N-1; i++){
+            swapCount = 0;
+            for(int j=0; j<N-1-i; j++){
+                if(A[j] > A[j + 1]){
+                    swap(A, j, j+1);
+                    swapCount++;
                 }
             }
+            if(swapCount ==0) break;
         }
 
-        // 정렬된 배열 출력
-        for (int i = 0; i < N; i++) {
+        for (int i=0; i<N; i++){
             sb.append(A[i]).append("\n");
         }
 
         System.out.println(sb);
+    }
+
+    public static void swap(int[] A, int i, int j){
+        int temp = 0;
+        temp = A[i];
+        A[i] = A[j];
+        A[j] = temp;
     }
 }
