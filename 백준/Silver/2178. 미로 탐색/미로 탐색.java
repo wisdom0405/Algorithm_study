4d31f85316cb19aad40fe72bc30fabd6718c7808
jeong seul ki <135789383+wisdom0405@users.noreply.github.com>
2025-03-01 17:03:37 +0900

    [Silver I] Title: 미로 탐색, Time: 156 ms, Memory: 16388 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -10,22 +10,22 @@ public class Main {
     static int M;
     static int[][] A;
     static boolean[][] visited;
-    static int[] dx = {-1, 1, 0, 0}; // 상, 하, 좌, 우
+    static int[] dx = {-1, 1, 0, 0}; // 상 하 좌 우 좌표
     static int[] dy = {0, 0, -1, 1};
 
-    public static void main (String[] args) throws IOException{
+    public static void main (String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        A = new int[N+1][M+1]; // (1,1) 부터 시작
-        visited = new boolean[N+1][M+1]; // 방문여부
+        A = new int[N+1][M+1]; // (1,1)부터 시작
+        visited = new boolean[N+1][M+1];
 
-        // 원본 배열 값 넣기
+        // 원본배열 값 입력받기 (미로)
         for(int i=1; i<N+1; i++){
-            String[] line = br.readLine().split(""); // 한 글자씩 나눠서 배열로 저장
+            String[] line = br.readLine().split("");
             for(int j=1; j<M+1; j++){
                 A[i][j] = Integer.parseInt(line[j-1]);
             }
@@ -33,30 +33,36 @@ public class Main {
 
         BFS(1,1);
         System.out.println(A[N][M]);
+
     }
 
     static void BFS(int i, int j){
-        Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[] {i, j});
-        visited[i][j] = true; // 방문 체크
+        Queue<int[]> queue = new LinkedList<>(); // 큐 선언
+        queue.add(new int[] {i,j}); // 시작점 좌표 넣기
+        visited[i][j] = true; // 방문체크
 
         while (!queue.isEmpty()){
-            int[] next = queue.poll(); // 다음탐색 좌표 (x,y)
-            for(int n=0; n<4; n++){
-                int x = next[0] + dx[n]; // 다음좌표 상하좌우 탐색
-                int y = next[1] + dy[n];
+            // 큐가 빌 때 까지 반복
+            int[] now = queue.poll(); // 이번좌표 : queue에서 poll한 값
 
-                if(x >=1 && y >=1 && x <= N && y <= M){ // 상하좌우 좌표가 범위를 벗어나지 않음
-                    // x,y 좌표가 막힌길이 아니고 방문한적 없다면
+            for(int n=0; n<4; n++){
+                int x = now[0] + dx[n]; // 이번좌표의 상하좌우 좌표 검사
+                int y = now[1] + dy[n];
+
+                // 상하좌우 좌표 유효성 검사 (범위 내에 있는지)
+                if(x>0 && y>0 && x<=N && y<=M ){
+                    // 막힌 길이 아니고 방문한 적 없는 좌표라면 queue에 add
                     if(A[x][y]!=0 && !visited[x][y]){
-                        visited[x][y] = true; // 방문체크
-                        queue.offer(new int[] {x, y}); // 해당좌표 큐에 넣기
-                        A[x][y] = A[next[0]][next[1]] + 1;
+                        visited[x][y] = true;
+                        queue.add(new int[] {x,y});
+
+                        // 현재좌표값 + 1한 값으로 update
+                        A[x][y] = A[now[0]][now[1]] + 1;
                     }
                 }
-
             }
         }
 
     }
+
 }
