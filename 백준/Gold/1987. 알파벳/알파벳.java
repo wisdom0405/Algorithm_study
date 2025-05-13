88bbe66e94d7c0693bff9bc7cc6b224c6dfd265c
jeong seul ki <135789383+wisdom0405@users.noreply.github.com>
2025-05-13 17:08:21 +0900

    [Gold IV] Title: 알파벳, Time: 864 ms, Memory: 14892 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -1,54 +1,48 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashSet;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int R,C;
-    static String[][] A;
-    static boolean[][] visited;
-    static int count = 0;
-    static HashSet<String> passed = new HashSet<>();
-    static int[] dx = {-1, 1, 0, 0}; // 상 하 좌 우 좌표
+    static int R,C,max = 0;
+    static char[][] board;
+    static boolean[] used = new boolean[26]; // A ~ Z 사용여부
+    static int[] dx = {-1, 1, 0, 0};
     static int[] dy = {0, 0, -1, 1};
+
     public static void main(String[] args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        R = Integer.parseInt(st.nextToken()); // 세로 칸 (행)
-        C = Integer.parseInt(st.nextToken()); // 가로 칸 (열)
-        A = new String[R+1][C+1]; // 보드 (행, 열 모두 1부터 시작)
-        visited = new boolean[R+1][C+1]; // 방문배열
+        R = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+        board = new char[R][C];
 
-        // 보드 정보 입력받기
-        for (int i=1; i<R+1; i++){
-            String[] alphabets = br.readLine().split("");
-            for (int j=1; j<C+1; j++){
-                A[i][j] = alphabets[j-1];
+        for (int i=0; i<R; i++){
+            String alphabets = br.readLine();
+            for (int j=0; j<C; j++){
+                board[i][j] = alphabets.charAt(j);
             }
         }
 
-        DFS(1,1);
-
-        System.out.println(count);
+        used[board[0][0] - 'A'] = true; // 첫 시작 알파벳 방문처리
+        DFS(0,0,1);
+        System.out.println(max);
     }
-    private static void DFS(int i, int j){
-        passed.add(A[i][j]); // 현재 알파벳 추가
-        count = Math.max(count, passed.size()); // 이동칸수 최대값으로 업데이트
+    private static void DFS(int x, int y, int depth){
+        // 이동칸 수 최댓값 비교
+        max = Math.max(max, depth);
 
-        for (int k=0; k<4; k++){ // 현재 좌표에서 상하좌우 좌표 탐색
-            int nx = i + dx[k];
-            int ny = j + dy[k];
-
-            if (isIn(nx,ny) && !visited[nx][ny] && !passed.contains(A[nx][ny])){
-                visited[nx][ny] = true;
-                DFS(nx, ny); // 탐색좌표로 재귀호출
-                visited[nx][ny] = false;
+        for (int k=0; k<4; k++){ // 현재 좌표 상하좌우 좌표 탐색
+            int nx = x + dx[k];
+            int ny = y + dy[k];
+            if (nx>=0 && ny>=0 && nx<R && ny<C){
+                int idx = board[nx][ny] - 'A'; // 탐색좌표 알파벳 index
+                if (!used[idx]){
+                    used[idx] = true;
+                    DFS(nx, ny, depth+1);
+                    used[idx] = false; // 백트래킹
+                }
             }
         }
-        passed.remove(A[i][j]); // 백트래킹
-    }
-    private static boolean isIn(int x, int y){
-        return x>0 && y>0 && x<=R && y<=C;
     }
 }
