f273b4c95f4a5f94be2c64cfcbb958301dbf480c
jeong seul ki <135789383+wisdom0405@users.noreply.github.com>
2025-06-01 19:10:07 +0900

    [Silver III] Title: 가희와 키워드, Time: 2468 ms, Memory: 226464 KB -BaekjoonHub

---
+++ 백준/Silver/22233. 가희와 키워드/가희와 키워드.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken()); // 가희가 메모장에 적은 키워드 개수
+        int M = Integer.parseInt(st.nextToken()); // 가희가 블로그에 쓴 글의 개수
+
+        // 메모장에 키워드 저장
+        HashSet<String> set = new HashSet<>();
+        while (N-->0){
+            set.add(br.readLine());
+        }
+
+        while (M-->0){
+            st = new StringTokenizer(br.readLine(),","); // ,(쉼표 구분)
+            while (st.hasMoreTokens()){
+                String keyword = st.nextToken();
+                set.remove(keyword);
+            }
+            System.out.println(set.size());
+        }
+    }
+}
